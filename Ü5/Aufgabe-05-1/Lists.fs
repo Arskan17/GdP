62c26b349c1318cef606697908d9475a0da51655
Arskan17 <75216911+Arskan17@users.noreply.github.com>
2020-12-04 23:25:15 +0100

    Ü5, Aufgabe 1

--- Ü5/Aufgabe-05-1/Lists.fs
+++ Ü5/Aufgabe-05-1/Lists.fs
@@ -6,50 +6,56 @@ let ex = [2N; 4N; 3N; 4N; 2N; 1N]
  
 // a)
 let rec concat<'a> (xs: List<'a>) (ys: List<'a>): List<'a> =
-    failwith "match xs with
-        [] -> ys
-        x::zs -> x::concat<'a> zs ys"
+    match xs with
+        |[] -> ys
+        |x::zs -> x::concat zs ys
  
 // b)
 let rec mirror<'a> (xs: List<'a>): List<'a> =
-   failwith "match xs with
-        [] -> []
-        x::ys -> concat<'a> (mirror<'a> ys) x::[]"
+    match xs with
+    | [] -> []
+    | x::ys -> concat (mirror ys) (x::[])
  
 // c)
 let rec digits (x: Nat): List<Nat> =
-   failwith "if x = 0N then []
-    else x % 10n :: digits x/10N"
+    if x = 0N then []
+    else (x%10N)::digits(x/10N)
  
 // d)
 let rec sum (xs: List<Nat>): Nat =
-    failwith "match xs with
-        [] -> 0N
-        x::ys -> x + sum ys"
- 
+    match xs with
+        |[] -> 0N
+        |x::ys -> x + sum ys
+  
 // e)
 let digitSum (x: Nat): Nat =
     sum (digits x)
  
 // f)
 let rec notUntil<'a when 'a: equality> (x: 'a) (xs: List<'a>): Nat =
-   failwith "match xs with
-        [] -> 0N
-        x::ys -> if x = y then 0N else 1N + notUntil<'a> x ys"
- 
+    match xs with
+        | []   -> 0N
+        | y::ys -> if x = y then 0N 
+                    else 1N + notUntil x ys 
+  
  
 // g)
 let rec map<'a> (f: 'a -> 'a) (xs: List<'a>): List<'a> =
-    failwith "match xs with
-        [] -> []
-        x::ys -> f x::map<'a> f ys"
+    match xs with
+        |[] -> []
+        |x::ys -> (f x) :: map f ys
  
 // h)
 let rec findAll<'a when 'a: equality> (x: 'a) (xs: List<'a>): List<Nat> =
-    failwith "TODO"
+    match xs with
+        | []    -> []
+        | y::ys -> let r = map (fun(n: Nat) -> 1N + n) (findAll x ys)
+                   if  x = y then 0N::r
+                   else r 
  
 // i)
 let rec findFirst<'a> (f: 'a -> Bool) (xs: List<'a>): Option<'a> =
-    failwith "match xs with
-        [] -> None
-        x::ys -> if f x then Some x else findFirst<'a> f ys"
+    match xs with
+        | []    -> None
+        | y::ys -> if f y then Some y 
+                    else findFirst f ys
